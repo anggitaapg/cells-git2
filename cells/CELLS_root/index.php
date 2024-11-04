a5a364bf8fbd25e8b82b7fe57b76ae1152756905
dareyouuu <raihanakbar1717@gmail.com>
2024-11-04 15:42:15 +0700

    typo article

--- cells/CELLS_root/index.php
+++ cells/CELLS_root/index.php
@@ -87,39 +87,39 @@ $other_news = array_slice($news_data, 5); // Sisanya untuk Others
     <?php include "header.php" ?>
 
    <!-- Carousel Start -->
-    <div class="container-fluid p-0 mb-5">
-        <div class="owl-carousel header-carousel position-relative">
-            <?php foreach($slides as $slide): ?>
-                <div class="owl-carousel-item position-relative">
-                    <img class="img-fluid" src="CELLS_Admin/admin/master/carousel/Fotocarousel/<?php echo htmlspecialchars($slide['image']); ?>"
-                        alt="<?php echo htmlspecialchars($slide['title']); ?>">
-                    <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center"
-                        style="background: rgba(24, 29, 56, .7);">
-                        <div class="container">
-                            <div class="row justify-content-start">
-                                <div class="col-sm-10 col-lg-8">
-                                    <h5 class="text-primary text-uppercase mb-3 animated slideInDown">
-                                        <?php echo date('F jS, Y', strtotime($slide['created_at'])); ?>
-                                    </h5>
-                                    <h1 class="display-3 text-white animated slideInDown">
-                                        <?php echo htmlspecialchars($slide['title']); ?>
-                                    </h1>
-                                    <p class="fs-5 text-white mb-4 pb-2">
-                                        <?php echo htmlspecialchars($slide['description']); ?>
-                                    </p>
-                                    <a href="article.php?id=<?php echo $slide['id']; ?>"
-                                    class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">
-                                        Read More
-                                    </a>
-                                </div>
+<div class="container-fluid p-0 mb-5">
+    <div class="owl-carousel header-carousel position-relative">
+        <?php foreach($slides as $slide): ?>
+            <div class="owl-carousel-item position-relative">
+                <img class="img-fluid" src="CELLS_Admin/admin/master/carousel/Fotocarousel/<?php echo htmlspecialchars($slide['image']); ?>"
+                    alt="<?php echo htmlspecialchars($slide['title']); ?>">
+                <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center"
+                    style="background: rgba(24, 29, 56, .7);">
+                    <div class="container">
+                        <div class="row justify-content-start">
+                            <div class="col-sm-10 col-lg-8">
+                                <h5 class="text-primary text-uppercase mb-3 animated slideInDown">
+                                    <?php echo date('F jS, Y', strtotime($slide['created_at'])); ?>
+                                </h5>
+                                <h1 class="display-3 text-white animated slideInDown">
+                                    <?php echo htmlspecialchars($slide['title']); ?>
+                                </h1>
+                                <p class="fs-5 text-white mb-4 pb-2">
+                                    <?php echo htmlspecialchars($slide['description']); ?>
+                                </p>
+                                <a href="<?php echo htmlspecialchars($slide['link_destination']); ?>"
+                                class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">
+                                    Read More
+                                </a>
                             </div>
                         </div>
                     </div>
                 </div>
-            <?php endforeach; ?>
-        </div>
+            </div>
+        <?php endforeach; ?>
     </div>
-    <!-- Carousel End -->
+</div>
+<!-- Carousel End -->
 
     <!-- Highlight Start -->
     <div class="container-xxl py-5 category">
